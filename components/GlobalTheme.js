5bd26a924c8e3fb60eeb49be16e405647ecda56e
Davis Cabrera <davis.cabrera@gmail.com>
2020-02-12 10:44:45 -0800

    scrollbar/minor changed

--- components/GlobalTheme.js
+++ components/GlobalTheme.js
@@ -5,29 +5,28 @@ export default createGlobalStyle`
         background: white;
         margin: 0 !important;
         font-family: 'Roboto', sans-serif;
-    }
-    
-    .BrainhubCarousel__arrowLeft {
-        background: #4630EB;
-        margin-left: 200px;
-    }
-    
-    .BrainhubCarousel__arrowRight {
-        background: #4630EB;
-        margin-right: 200px;
-    }
-    
-    ::-webkit-scrollbar {
-        width: 9px;
-    }
-    
-    ::-webkit-scrollbar-track {
-    background: #f1f1f1; 
-    }
- 
 
-    ::-webkit-scrollbar-thumb {
-    background: #4630EB; 
-    }
-      
+        .BrainhubCarousel__arrowLeft {
+            background: #4630EB;
+            margin-left: 200px;
+        }
+        
+        .BrainhubCarousel__arrowRight {
+            background: #4630EB;
+            margin-right: 200px;
+        }
+        
+        ::-webkit-scrollbar {
+            width: 9px;
+        }
+        
+        ::-webkit-scrollbar-track {
+        background: #f1f1f1; 
+        }
+     
+    
+        ::-webkit-scrollbar-thumb {
+        background: #4630EB; 
+        }
+} 
 `
